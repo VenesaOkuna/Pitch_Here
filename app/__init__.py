8572d31f656b546b1486552bc809d2a9d1ed95f9
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-02-12 13:55:32 +0300

    add login system

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,14 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import DevConfig, config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 
 
-# login_manager = LoginManager()
-# login_manager.session_protection = 'strong'
-# login_manager.login_view = 'auth.login'
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 
@@ -30,7 +31,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
-    # login_manager.init_app(app)
+    login_manager.init_app(app)
     # mail.init_app(app)
 
 
